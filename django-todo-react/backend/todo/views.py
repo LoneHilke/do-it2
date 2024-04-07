33a86dc74445608185a4baea8dd3e1e98a7b372e
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-04-07 21:53:00 +0200

    kommet til step 3
    API virker
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- django-todo-react/backend/todo/views.py
+++ django-todo-react/backend/todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+
+
+
+
+#fra: https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react#step-1-setting-up-the-backend
